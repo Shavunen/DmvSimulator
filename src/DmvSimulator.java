88432a9a3e1acb08753a08418877b1a7ee24c882
Shavunen <snhavunen@quinnipiac.edu>
2022-03-06 15:58:37 -0500

    setup

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,4 +1,34 @@
 
-public class DmvSimulator {
 
+public class DmvSimulator {
+	public static void main(String[] args) {
+
+		System.out.println("Wecolcome to the DMV");
+
+		for (int x = 1; x >= 100; x++); 
+			
+		{
+			int User = (int) (Math.random() * 100);
+
+			System.out.println("This is your number in line:" + " " + User);
+
+			System.out.println("Please wait until your number is called. Thank you");
+
+			int Wait = 1 + User;
+
+			while (User != Wait) {
+				System.out.println("Calling:" + " " + Wait);
+				Wait++;
+				if (Wait == 101) {
+					Wait = 1;
+				}
+			}
+
+			System.out.println("Now calling:" + " " + Wait);
+
+			System.out.println("Im sorry, you do not have the required paperwork. Please come back another day");
+
+		}
+
+	}
 }
